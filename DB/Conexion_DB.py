f2bf6ef5ba74eae638976488131252012fc616fa
unknown <ymvelasquezr@uqvirtual.edu.co>
2019-10-22 19:39:53 -0500

    actualizacion

--- DB/Conexion_DB.py
+++ DB/Conexion_DB.py
@@ -1,15 +1,14 @@
 import psycopg2
 
+##def abrir():
+##    conexion = psycopg2.connect(database="trabajoFinal", user="postgres", password="12345")
+##    return conexion
 
-def abrir(self):
-    conexion = psycopg2.connect(database="trabajoFinal", user="postgres", password="12345")
-    return conexion
 
-
-def alta(self):
-    cone = self.abrir()
-    cursor = cone.cursor()
-    datos = ("1094970055", "meliza", "velasuqez", "3146126046", "armenia")
-    sql = "insert into docente(docente_id, nombre, apellido, telefono, ciudad) values (%s,%s,%s, %s,%s)"
-    cursor.execute(sql, datos)
-    cone.commit()
+##def alta():
+##    cone = abrir()
+##    cursor = cone.cursor()
+##    datos = ("1094970055", "meliza", "velasuqez", "3146126046", "armenia")
+#    sql = "insert into docente(docente_id, nombre, apellido, telefono, ciudad) values (%s,%s,%s, %s,%s)"
+#    cursor.execute(sql, datos)
+#    cone.commit()
