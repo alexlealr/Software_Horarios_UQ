f49b18572d04b3e93c8dff6a9ffb2f71cd303a30
unknown <ymvelasquezr@uqvirtual.edu.co>
2019-12-21 16:59:43 -0500

    create of conexion DB

--- GUI/Main.py
+++ GUI/Main.py
@@ -1,13 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from GUI.Login import Login
-from DB.Conexion_DB import alta
+
 
 if __name__ == "__main__":
     app = QApplication([])
     hola = QMainWindow()
     main_window = Login()
     main_window.setup_ui(hola)
-    alta()
     hola.show()
     sys.exit(app.exec_())
